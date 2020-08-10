7f027a01dbfd0d8d2f6801d108ec59b65e0fd061
Julian Thome <jthome@gitlab.com>
2020-08-10 09:52:59 +0200

    patching functionality

--- dwmgr.sh
+++ dwmgr.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
 # Configuration parameters
+
+set -ex pipefail
+
 PERSONAL_DWM_REPO="my-dwm"
 PATCHES_PATH="patches"
 DWM_URL="dwm.suckless.org"
@@ -6,19 +10,20 @@ PATCH_PATH="${DWM_URL}/${PATCHES_PATH}"
 PATCHES_URL="https://${PATCH_PATH}/"
 DWM_VERSION="6.2"
 
-function clone_repo() { 
+clone_repo() { 
     [ -d "$PERSONAL_DWM_REPO" ] || {
-        git clone https://git.suckless.org/dwm --depth 1 "$PERSONAL_DWM_REPO"
+        git clone https://git.suckless.org/dwm "$PERSONAL_DWM_REPO"
+        #(cd "$PERSONAL_DWM_REPO" && git checkout "$DWM_VERSION")
     }
 }
 
-function download_patches() {
+download_patches() {
     wget -q --recursive ‐-mirror \
         --wait=15 --limit-rate=50K \
         --domains "${DWM_URL}" --no-parent "${PATCHES_URL}"
 }
 
-function prepare_repo() { 
+prepare_repo() { 
     clone_repo
     cd "$PERSONAL_DWM_REPO"
     find "../${PATCH_PATH}" \
@@ -27,26 +32,46 @@ function prepare_repo() {
         -maxdepth 1 \
         -name "historical" -prune \
         -o -print | while read -r extension; do
-            echo "Feature: $(basename $extension)"
+            echo "Feature: $(basename "$extension")"
 
-            find "$extension" -type f -name "dwm-*${DWM_VERSION}.diff" \
-                | sort | uniq | while read -r patch; do
-                echo "Patch: $patch"
-                BASE=$(basename ${patch%.*})
-                PATCH_NAME=$(echo $BASE | cut -d '-' -f 2)
+            find "$extension" \
+                -type f \
+                -name "dwm-*${DWM_VERSION}.diff" \
+                | sort \
+                | uniq \
+                | while read -r patch; do
+               
+                BASE=$(basename "${patch%.*}")
+                PATCH_NAME=$(echo "$BASE" | cut -d '-' -f 2)
                 BRANCH_NAME="$PATCH_NAME"
-                FEATURE_NAME=$(echo $BASE | cut -d '-' -f 3 | grep -v "$DWM_VERSION")
-                [ -n "$FEATURE_NAME" ] && {
+                FEATURE_NAME=$(echo "$BASE" | cut -d '-' -f 3)
+                
+                [ "$FEATURE_NAME" = "$DWM_VERSION" ] || {
                     BRANCH_NAME="${BRANCH_NAME}_${FEATURE_NAME}"
                 }
-                echo "$BRANCH_NAME" 
+
+                EXISTGING_BRANCH=$(git branch -l "$BRANCH_NAME")
+
+                [ "$EXISTGING_BRANCH" = "$BRANCH_NAME" ] && {
+                    echo "branch $EXISTGING_BRANCH already present - skipping" 
+                    continue
+                }
+
+                echo "apply patch $BASE on branch $BRANCH_NAME"
                 git checkout -b "$BRANCH_NAME"
-                git apply $patch || exit 1
+
+                patch -s -t -p1 -F 3 < "$patch" || {
+                    echo "Could not apply patch $patch"     
+                    echo "returning"
+                    exit 1
+                }
+
+                git add -A                
+                git commit -m "applied patch"
                 git checkout origin/master
-                #BRANCH=$(echo $BASE  | tr -d '.' | tr '/' '_')
-                #echo "Branch: $BRANCH"
             done
     done
+    return 0
 }
 
 prepare_repo
